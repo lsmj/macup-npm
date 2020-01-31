ec298820f54e496f6ff4c7616410bd1995b27f33
Lyder Stefan Minde Janøy <lyder@animidas.no>
2020-01-31 13:06:41 +0100

    dropped auto-installing NodeJS

--- dist/run.sh
+++ dist/run.sh
@@ -7,22 +7,18 @@ function install_node_package {
   npm -g install "$package"
 }
 
-if [ ! ${#macup_node_packages[@]} -eq 0 ]; then # If config has npm entries
-  # Checking if NodeJS is installed
-  if [ ! "$(command -v node)" ]; then # If NodeJS is not installed
-    report_from_package " WARNING: NodeJS is not installed. Installing.."
-    if [ ! "$(command -v brew)" ]; then # If Homebrew is not installed
-      report_from_package " WARNING: Homebrew is not installed. Installing.."
-      ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-    fi
-    brew install node
+if [ ! ${#macup_node_packages_npm[@]} -eq 0 ]; then
+  if [ "$(command -v node)" ]; then
+    for ((i=0; i<${#macup_node_packages_npm[@]}; ++i)); do
+      install_node_package "${macup_node_packages_npm[i]}"
+    done
+    report_from_package " Global NodeJS packages:"
+    npm list -g --depth 0
+    report_from_package " Outdated global NodeJS packages:"
+    npm outdated -g
+  else
+    report_from_package " NodeJS is not installed. Install NodeJS and restart macup."
   fi
-  # Install Node packages
-  for ((i=0; i<${#macup_node_packages[@]}; ++i)); do
-    install_node_package "${macup_node_packages[i]}"
-  done
-  report_from_package " Global NodeJS packages:"
-  npm list -g --depth 0
-  report_from_package " Outdated global NodeJS packages:"
-  npm outdated -g
+else
+  report_from_package " WARNING: No items in the config file"
 fi
